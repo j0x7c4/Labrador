a4297f23ebcbb321b4d9a303ab5efd1321d9e16e
Jie <china.eli@gmail.com>
2013-09-07 01:58:53 +0800

    update

---
+++ src/memstorage.py
@@ -0,0 +1,9 @@
+__author__ = 'Seeker'
+
+weibos_md5_dict = {"uid" : "md5", }
+comments_md5_dict = {"mid" : "md5", }
+users_id_moniterd = ['1881798702','1739412814']
+uids_url_moniterd = []
+uids_url2_moniterd = []
+weibos_url_moniterd = []
+
